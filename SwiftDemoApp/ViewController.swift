9383c39a46368a91efce144584d4fe5ca0fce8ea
Derk Gommers <ik@derkgommers.eu>
2017-06-22 20:11:52 +0200

    Created initial Xcode project

---
+++ SwiftDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemoApp
+//
+//  Created by Derk Gommers on 22/06/2017.
+//  Copyright © 2017 Derk Gommers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
