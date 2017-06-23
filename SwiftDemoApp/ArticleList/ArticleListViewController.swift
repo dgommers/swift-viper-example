1cc1e3d7b062be525a85ade16d3ffb54bf00743f
Derk Gommers <ik@derkgommers.eu>
2017-06-23 21:55:02 +0200

    Added view will appear event handling to the view controller

--- SwiftDemoApp/ArticleList/ArticleListViewController.swift
+++ SwiftDemoApp/ArticleList/ArticleListViewController.swift
@@ -2,9 +2,23 @@
 
 import UIKit
 
-class ArticleListViewController: UITableViewController {
+protocol ArticleListView: class {
+    var viewModel: ArticleListViewModel? { get set }
+}
+
+protocol ArticleListEventHandler {
+    func viewWillAppear()
+}
+
+class ArticleListViewController: UITableViewController, ArticleListView {
 
     var viewModel: ArticleListViewModel?
+    var eventHandler: ArticleListEventHandler?
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        eventHandler?.viewWillAppear()
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel?.articles.count ?? 0
