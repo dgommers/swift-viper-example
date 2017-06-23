210ebd03d3f229d633a39258fe7780bc78201380
Derk Gommers <ik@derkgommers.eu>
2017-06-23 20:23:09 +0200

    First view model that only contains article title

---
+++ SwiftDemoApp/ArticleList/ArticleListViewModel.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2017 Derk Gommers. All rights reserved.
+
+struct ArticleListViewModel {
+    var articles: [ArticleRowViewModel]
+}
+
+struct ArticleRowViewModel {
+    var title: String
+}
