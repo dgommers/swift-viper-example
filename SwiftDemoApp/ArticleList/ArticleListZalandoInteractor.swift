134e16080e0bf247b8f399d5cab706c6905309e0
Derk Gommers <ik@derkgommers.eu>
2017-06-25 16:45:34 +0200

    Extracted hostname to local constant

--- SwiftDemoApp/ArticleList/ArticleListZalandoInteractor.swift
+++ SwiftDemoApp/ArticleList/ArticleListZalandoInteractor.swift
@@ -9,9 +9,10 @@ protocol ArticleListInteractor {
 struct ArticleListZalandoInteractor: ArticleListInteractor {
 
     var session: URLSessionType = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
+    private let host = "api.zalando.com"
 
     func articles(completion: @escaping ([String]) -> Void) {
-        let url = URL(string: "https://api.zalando.com/articles?fields=name")!
+        let url = URL(string: "https://\(host)/articles?fields=name")!
         session.request(with: url) { data, _, _ in
             let root = data?.json as? [String: Any]
             let content = root?["content"] as? [[String: String]]
