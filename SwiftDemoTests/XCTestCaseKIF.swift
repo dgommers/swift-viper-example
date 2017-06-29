79ade04f9be55d0905b574d37c912b1bf0f74d81
Derk Gommers <ik@derkgommers.eu>
2017-06-29 10:49:26 +0200

    Added reference screenshotting

--- SwiftDemoTests/XCTestCaseKIF.swift
+++ SwiftDemoTests/XCTestCaseKIF.swift
@@ -1,9 +1,17 @@
 //  Copyright © 2017 Derk Gommers. All rights reserved.
 
 import KIF
+import Quick
 
 extension XCTestCase {
     func tester(file: String = #file, _ line: Int = #line) -> KIFUIViewTestActor {
         return KIFUIViewTestActor(inFile: file, atLine: line, delegate: self)
+    }
+
+    func capture(name: String, file: String = #file, _ line: Int = #line) {
+        let actor = KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+        it("captures screen \(name)") {
+            actor?.captureScreenshot(withDescription: name)
+        }
     }
 }
