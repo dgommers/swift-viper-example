e822330d1ed40030425376bab1d26ad5e60509c9
Derk Gommers <ik@derkgommers.eu>
2017-06-29 08:18:19 +0200

    Changing color for unavailable units

--- SwiftDemoApp/ArticleList/Presenter/ArticleListItemPresenter.swift
+++ SwiftDemoApp/ArticleList/Presenter/ArticleListItemPresenter.swift
@@ -1,20 +1,31 @@
 //  Copyright © 2017 Derk Gommers. All rights reserved.
 
-import Foundation
+import UIKit
 
 protocol ArticleListItemPresenterType {
     func viewModel(article: Article) -> ArticleListItemViewModel
 }
 
 struct ArticleListItemPresenter: ArticleListItemPresenterType {
+
+    let unavailableAttributes = [
+        NSForegroundColorAttributeName: UIColor.lightGray
+    ]
+
     func viewModel(article: Article) -> ArticleListItemViewModel {
         return ArticleListItemViewModel(
             name: article.name,
             price: article.units?.first?.price?.formatted,
             image: article.media?.images?.first?.smallHdURL,
-            units: article.units?.flatMap {
-                NSAttributedString(string: $0.size ?? "")
-            }
+            units: article.units?.flatMap(unitAttributedText)
+        )
+    }
+
+    func unitAttributedText(unit: ArticleUnit) -> NSAttributedString? {
+        let available = unit.available ?? false
+        return NSAttributedString(
+            string: unit.size ?? "",
+            attributes: available ? [:] : unavailableAttributes
         )
     }
 }
